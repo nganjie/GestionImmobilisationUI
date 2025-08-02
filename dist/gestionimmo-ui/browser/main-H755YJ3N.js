10ce1ae919c98ea6d442df08ed6dd9090c783c5c
nganjie nzatsi <nganjienzatsi@gmail.com>
2025-08-02 02:14:03 +0100

    add build app

---
+++ dist/gestionimmo-ui/browser/main-H755YJ3N.js
@@ -0,0 +1,1 @@
+import{Tc as r,sb as o}from"./chunk-DX4DG724.js";o().bootstrapModule(r,{ngZoneEventCoalescing:!0}).catch(t=>console.error(t));
--- dist/gestionimmo-ui/browser/main-TNJZQUER.js
+++
@@ -1,1 +0,0 @@
-import{Tc as r,sb as o}from"./chunk-EE2NSQVY.js";o().bootstrapModule(r,{ngZoneEventCoalescing:!0}).catch(t=>console.error(t));
