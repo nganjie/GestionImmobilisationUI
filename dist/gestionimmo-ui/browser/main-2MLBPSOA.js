0788c65c35005452ba47e36a6fad671ef73423f4
nganjie nzatsi <nganjienzatsi@gmail.com>
2025-08-13 23:05:21 +0100

    update ui

---
+++ dist/gestionimmo-ui/browser/main-2MLBPSOA.js
@@ -0,0 +1,1 @@
+import{Tc as r,sb as o}from"./chunk-X6EDCZ5O.js";o().bootstrapModule(r,{ngZoneEventCoalescing:!0}).catch(t=>console.error(t));
--- dist/gestionimmo-ui/browser/main-PRPE6EBQ.js
+++
@@ -1,1 +0,0 @@
-import{Tc as r,sb as o}from"./chunk-AQ6AXN4A.js";o().bootstrapModule(r,{ngZoneEventCoalescing:!0}).catch(t=>console.error(t));
